25ba6e52cae09bfb799c283349d67b6d41683a67
LordSMG <giltonkarso2015@gmail.com>
2025-08-24 04:37:55 +0000

    Edited Spark

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export function TestComponent() {
   return (
-    <div className="p-4">
+      <p>This is a test c
       <h2 className="text-lg font-semibold">Test Component</h2>
       <p>This is a test component.</p>
     </div>
-  )
+
 }
