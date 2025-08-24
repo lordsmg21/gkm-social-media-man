0370485c0a6de799542ab58db8f9e0075d83832e
LordSMG <giltonkarso2015@gmail.com>
2025-08-24 03:21:11 +0000

    Edited Spark

---
+++ src/TestComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function TestComponent() {
+  const testData = {
+  return (
+      <h1 classN
+   
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Test Component</h1>
+      <p>Value: {testData.value}</p>
+      <p>Formatted: {testData.value.toString()}</p>
+    </div>
+  )
+}
